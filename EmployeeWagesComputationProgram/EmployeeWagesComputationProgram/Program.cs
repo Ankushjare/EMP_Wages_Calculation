a0f5d26dcf67f7cce4a311dfd505d33b0b4c407c
Ankushjare <jareankush@gmail.com>
2021-10-22 00:23:02 +0530

    Add_Deily_emp_wage_calculate

--- EmployeeWagesComputationProgram/EmployeeWagesComputationProgram/Program.cs
+++ EmployeeWagesComputationProgram/EmployeeWagesComputationProgram/Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 
-namespace EmployeeWagesComputationProgram
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int empPerHrs = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
 
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("present");
+            {
+                Console.WriteLine("Present");
+                empHrs = 8;
+            }
             else
+            {
                 Console.WriteLine("Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * empPerHrs;
+            Console.WriteLine(+empWage);
+
         }
     }
 }
