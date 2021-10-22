bc752490b2cc7b6dd0c6b219cd134132952c7a56
Ankushjare <jareankush@gmail.com>
2021-10-22 10:23:42 +0530

    Add:- Caculate Wages for Month

--- EmployeeWagesComputationProgram/EmployeeWagesComputationProgram/Program.cs
+++ EmployeeWagesComputationProgram/EmployeeWagesComputationProgram/Program.cs
@@ -4,64 +4,74 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
+            const int empPerHrs = 20;
+            const int No_Working_days = 20;
 
-            int empPerHrs = 20;
-
-            int empHrs = 0;
-            int empWage = 0;
-
-
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            static void Main(string[] args)
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Full Time");
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    Console.WriteLine("Part Time");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Absent");
-                    empHrs = 0;
-                    break;
+
+
+
+                int empHrs = 0;
+                int empWage = 0;
+                int totalEmpWages = 0;
+                for (int i = 0; i < No_Working_days; i++)
+                {
+
+                    Random random = new Random();
+
+                    int empCheck = random.Next(0, 3);
+
+                    switch (empCheck)
+                    {
+                        case IS_FULL_TIME:
+                            Console.WriteLine("Full Time");
+                            empHrs = 8;
+                            break;
+                        case IS_PART_TIME:
+                            Console.WriteLine("Part Time");
+                            empHrs = 4;
+                            break;
+                        default:
+                            Console.WriteLine("Absent");
+                            empHrs = 0;
+                            break;
+                    }
+
+                    /*if (empCheck == IS_FULL_TIME)
+                    {
+                        empHrs = 8;
+                        Console.WriteLine("Full Time");
+                    }
+                    else if (empCheck == IS_PART_TIME)
+                    {
+                        empHrs = 4;
+                        Console.WriteLine("Part Time");
+                    }
+                    else
+                    {
+                        empHrs = 0;
+                        Console.WriteLine("Absent");
+                    }
+
+                    if (empCheck == IS_FULL_TIME)
+                    {
+                        empHrs = 8;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Absent");
+                    }*/
+                    empWage = empHrs * empPerHrs;
+                    totalEmpWages = totalEmpWages + empWage;
+                    Console.WriteLine("Employee wages is=" + empWage);
+                }
+                Console.WriteLine("total employee wage is=" + totalEmpWages);
+
             }
 
-            /*if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-                Console.WriteLine("Full Time");
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-                Console.WriteLine("Part Time");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Absent");
-            }*/
-
-            /*if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Absent");
-            }*/
-            empWage = empHrs * empPerHrs;
-            Console.WriteLine(+empWage);
-
-        }
+        
     }
 }
